21a465df3d185f6d9f832bdcd84bb22ce27ae9a9
stud <stud@it224-6.almetpt.local>
2025-03-15 12:52:54 +0300

    Auto-commit: 2025-03-15 12:52:54

--- push.sh
+++ push.sh
@@ -1,18 +1,13 @@
-
 REPO_URL="git@github.com:jutfruze/kursach_oil.git"
 BRANCH="main"
 COMMIT_MESSAGE="Auto-commit: $(date +'%Y-%m-%d %H:%M:%S')"
 
-# Добавляем все изменения
 git add .
 
-# Делаем коммит
 git commit -m "$COMMIT_MESSAGE"
 
-# Пушим изменения
-git push $REPO_URL $BRANCH
+git push origin $BRANCH
 
-# Проверяем результат
 if [ $? -eq 0 ]; then
     echo "Изменения успешно отправлены в репозиторий!"
 else
